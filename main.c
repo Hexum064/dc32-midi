1d60ef31d44ebfc83ce192b5a1e26b87b96c5b63
BrandenBoucher <bgbouche@asu.edu>
2024-05-10 18:39:55 -0700

    Fixed samples and pitching is working

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ bool playing_sample = false;
 
 float step = 0;
 
-#define US_FOR_11K 3 //91
+#define US_FOR_11K 2 //91
 
 uint8_t InterpolateHermite4pt3oX(uint8_t* x, uint32_t t)
 {
@@ -180,18 +180,29 @@ static void alarm_2_irq(void) {
     {
         uint64_t start_time;
         uint64_t end_time;
+
+        
         // start_time = time_us_64();
         //TODO: Might need to load the sample in local mem
-        uint16_t d = *(voices[0].sample_data + (uint16_t)voices[0].offset) << 8; // [(uint16_t)sample_offset] << 8;//  samples_start[sample_offset] << 8;        
+        uint16_t d = *(voices[0].sample_data + (uint16_t)(voices[0].offset>>14)) << 8; // [(uint16_t)sample_offset] << 8;//  samples_start[sample_offset] << 8;        
         // i2c_data[1] = d;
         // i2c_write_blocking(AUDIO_I2C, AUDIO_I2C_ADDR, i2c_data, 3, true );
         gpio_put_masked(0xFF00, d);
         // printf("**%u: 0x%04x.\n",sample_offset, d);
         //Send I2C: try i2c_write_byte_raw next
         
-        // voices[0].offset++;
-        voices[0].offset += voices[0].offset_step;
-        playing_sample = voices[0].offset < voices[0].sample_len;
+        //voices[0].offset++;
+         voices[0].offset += voices[0].offset_step;
+        // 
+        //voices[0].offset_step_cnt += voices[0].offset_step;
+        
+        //skip an offset (add another to the offset) if our offset step count went over 0x0FFFFFF
+        // if (voices[0].offset_step_cnt >> 8) // 0FFFFFFF instead of FFFFFFFF so the count can go over the target without overflowing
+        // {
+        //     voices[0].offset_step_cnt = 0;
+        //     voices[0].offset++;
+        // }
+        playing_sample = voices[0].offset < (uint32_t)(voices[0].sample_len << 14);
         voices[0].octave_div_cnt = voices[0].octave_div;
         // end_time = time_us_64();
         // printf("%llu\n", end_time-start_time);
@@ -256,7 +267,7 @@ void load_channel(uint8_t channel, uint8_t patch)
     //Offset is the start of the key data. Key data starts after the patch count and offsets for each patch
     uint16_t index = (patch * 2) + 1;
     uint16_t offset = (uint16_t)map_start[index] + (uint16_t)(map_start[index + 1] << 8);
-    // printf("Index 0x%04x, Data: 0x%04x\n", index, offset);
+    //printf("Index 0x%04x, Data: 0x%04x\n", index, offset);
     offset += (map_start[0] * 2) + 2; //2 instead of one to skip the patch id too
     uint8_t cnt = map_start[offset++];
     // printf("count: %d, offset: 0x%04x\n", cnt, offset);
@@ -303,7 +314,7 @@ void load_sample(uint8_t channel, uint8_t key, uint8_t velocity)
     uint8_t key_index = find_key(channel, key);
     key_info keyInfo = channels[channel].keys[key_index];    
     voices[some_index].root = map_start[keyInfo.key_offset+2];
-    uint8_t rate_index = 60 + (int8_t)(key - voices[some_index].root);
+    // uint8_t rate_index = 60 + (int8_t)(key - voices[some_index].root);
     voices[some_index].sample_len = (uint16_t)map_start[keyInfo.key_offset+7] + (uint16_t)(map_start[keyInfo.key_offset+8] << 8);
     voices[some_index].loop_start = (uint16_t)map_start[keyInfo.key_offset+9] + (uint16_t)(map_start[keyInfo.key_offset+10] << 8);
     // uint8_t * base_sample_data = (uint8_t *)malloc(base_len);
@@ -314,7 +325,7 @@ void load_sample(uint8_t channel, uint8_t key, uint8_t velocity)
     *(offset_ptr++) = map_start[keyInfo.key_offset+5];
     *(offset_ptr++) = map_start[keyInfo.key_offset+6];
     voices[some_index].sample_data = samples_start + sample_data_offset;
-    // printf("Offset: 0x%08x, base len: %llu\n", sample_data_offset, base_len);
+    // printf("Offset: 0x%08x, base len: %lu\n", sample_data_offset, voices[some_index].sample_len);
 
     // uint32_t rate = pitches[rate_index];
     
@@ -331,19 +342,41 @@ void load_sample(uint8_t channel, uint8_t key, uint8_t velocity)
         // pitch_sample(voices[some_index].sample_data, base_sample_data, rate, 0, base_len);
         // end_time = time_us_64();
         // printf("Pitch change time: %llu\n", end_time - start_time);
-    // pitch_sample(voices[some_index].sample_data, samples_start, rate, sample_data_offset, base_len);
-    // printf("New Len: %d, key: %d, root: %d, loop start: %d, rate: %lu, orig len: %llu, orig loop start: %llu\n", 
-    // voices[some_index].sample_len, key, voices[some_index].root, voices[some_index].loop_start, rate, base_len, base_loop_start);
+    // // pitch_sample(voices[some_index].sample_data, samples_start, rate, sample_data_offset, base_len);
+    // printf("New Len: %d, key: %d, root: %d, loop start: %d, orig len: %llu, orig loop start: %llu\n", 
+    // voices[some_index].sample_len, key, voices[some_index].root, voices[some_index].loop_start, voices[some_index].sample_len, voices[some_index].loop_start);
     
+    uint8_t root = voices[some_index].root;
+    uint8_t diff = 71 + (root-key);
+    uint8_t step_mod = (diff-12) % 12;
+    
+    uint8_t exponent = (int8_t)(diff / 12);  
+    uint8_t oct_div = 1 << exponent;
 
+    // int8_t diff = key-voices[some_index].root;    
+    // int8_t step_mod = diff % 12;
     
-    int8_t diff = key-voices[some_index].root;
-    int8_t step_index = diff % 12;
-    step_index = step_index < 0 ? step_index + 12 : step_index;
-    uint8_t div = 5 - ((diff - step_index) / 12);
-    voices[some_index].octave_div = 1 << div;
+    // uint8_t exponent = 0;
+    
+    // if (diff < 0)
+    // {
+    //     exponent = 6 - (uint8_t)((diff+1)/ 12);  
+    //     step_mod = -step_mod;
+    // }
+    // else
+    // {
+    //     exponent = 5 - (uint8_t)(diff / 12);  
+        
+        
+    // }
+    // printf("root: %d, key: %d, diff: %d, step_mod: %d, step: %lu, exponent: %d: sample offset: 0x%08x, len: %u\n", root, key, diff, step_mod, twelfths[step_mod], exponent, sample_data_offset, voices[some_index].sample_len);
+    
+    // div = diff < 0 ? div+1 : div;
+    
+    voices[some_index].octave_div = oct_div;
     voices[some_index].octave_div_cnt = voices[some_index].octave_div;
-    voices[some_index].offset_step = twelfths[step_index];
+    voices[some_index].offset_step = twelfths[step_mod];
+    voices[some_index].offset_step_cnt = 0;
     voices[some_index].offset = 0;
     // printf("Key: %d, lo: %d, hi: %d, root %d\n", key, channels[channel].keys[key_index].low_key, channels[channel].keys[key_index].high_key, voices[some_index].root);
     /*
@@ -395,7 +428,7 @@ void main2()
 {
     printf("Entering core 1 (%d)\n", get_core_num());
     printf("playing samples\n");
-    uint8_t patch = 0;
+    uint8_t patch = 0; //114;
 
     uint64_t start_time;
     uint64_t end_time;
@@ -425,7 +458,7 @@ void main2()
     // copy_from_flash(pitched_sample, sample_start, orig_len);
     // pitched_sample = (uint8_t *)malloc(1);
     // float rate;
-    for (int8_t i = 37; i < 87; i++)
+    for (int8_t i = 56 ; i < 84; i = i + 1 )
     {
         // playing_sample = true;
         // step = (float)i/12;
@@ -443,7 +476,7 @@ void main2()
         end_time = time_us_64();
 
         delta_time = end_time - start_time;        
-        printf("Key: %d load time: %llu. Offset: 0x%08x, oct div: %d, step: %f\n", i, delta_time, voices[0].sample_data, voices[0].octave_div, voices[0].offset_step);
+        printf("Key: %d load time: %llu. Offset: 0x%08x, oct div: %d, step: %lu.\n", i, delta_time, voices[0].sample_data, voices[0].octave_div, voices[0].offset_step);
         playing_sample = true;
         alarm_2_in_us(US_FOR_11K);
         
@@ -457,6 +490,8 @@ void main2()
 int main() {
     int i = 0;
     
+    set_sys_clock_khz(133000, true);
+
     dac_io_init();
 
     stdio_init_all();
@@ -475,98 +510,41 @@ sleep_ms(2000);
     printf("Starting...\n");
 
     puts("Hello, world!");
+    printf("---Patches: %d---\n", map_start[0]);
+    offset = 2 * map_start[0] + 1;
 
-bool count_down = false;
-uint8_t val = 0;
-uint8_t  sine_wave[256] = {
-  0x80, 0x83, 0x86, 0x89, 0x8C, 0x90, 0x93, 0x96,
-  0x99, 0x9C, 0x9F, 0xA2, 0xA5, 0xA8, 0xAB, 0xAE,
-  0xB1, 0xB3, 0xB6, 0xB9, 0xBC, 0xBF, 0xC1, 0xC4,
-  0xC7, 0xC9, 0xCC, 0xCE, 0xD1, 0xD3, 0xD5, 0xD8,
-  0xDA, 0xDC, 0xDE, 0xE0, 0xE2, 0xE4, 0xE6, 0xE8,
-  0xEA, 0xEB, 0xED, 0xEF, 0xF0, 0xF1, 0xF3, 0xF4,
-  0xF5, 0xF6, 0xF8, 0xF9, 0xFA, 0xFA, 0xFB, 0xFC,
-  0xFD, 0xFD, 0xFE, 0xFE, 0xFE, 0xFF, 0xFF, 0xFF,
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFE, 0xFE, 0xFD,
-  0xFD, 0xFC, 0xFB, 0xFA, 0xFA, 0xF9, 0xF8, 0xF6,
-  0xF5, 0xF4, 0xF3, 0xF1, 0xF0, 0xEF, 0xED, 0xEB,
-  0xEA, 0xE8, 0xE6, 0xE4, 0xE2, 0xE0, 0xDE, 0xDC,
-  0xDA, 0xD8, 0xD5, 0xD3, 0xD1, 0xCE, 0xCC, 0xC9,
-  0xC7, 0xC4, 0xC1, 0xBF, 0xBC, 0xB9, 0xB6, 0xB3,
-  0xB1, 0xAE, 0xAB, 0xA8, 0xA5, 0xA2, 0x9F, 0x9C,
-  0x99, 0x96, 0x93, 0x90, 0x8C, 0x89, 0x86, 0x83,
-  0x80, 0x7D, 0x7A, 0x77, 0x74, 0x70, 0x6D, 0x6A,
-  0x67, 0x64, 0x61, 0x5E, 0x5B, 0x58, 0x55, 0x52,
-  0x4F, 0x4D, 0x4A, 0x47, 0x44, 0x41, 0x3F, 0x3C,
-  0x39, 0x37, 0x34, 0x32, 0x2F, 0x2D, 0x2B, 0x28,
-  0x26, 0x24, 0x22, 0x20, 0x1E, 0x1C, 0x1A, 0x18,
-  0x16, 0x15, 0x13, 0x11, 0x10, 0x0F, 0x0D, 0x0C,
-  0x0B, 0x0A, 0x08, 0x07, 0x06, 0x06, 0x05, 0x04,
-  0x03, 0x03, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01,
-  0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x03,
-  0x03, 0x04, 0x05, 0x06, 0x06, 0x07, 0x08, 0x0A,
-  0x0B, 0x0C, 0x0D, 0x0F, 0x10, 0x11, 0x13, 0x15,
-  0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24,
-  0x26, 0x28, 0x2B, 0x2D, 0x2F, 0x32, 0x34, 0x37,
-  0x39, 0x3C, 0x3F, 0x41, 0x44, 0x47, 0x4A, 0x4D,
-  0x4F, 0x52, 0x55, 0x58, 0x5B, 0x5E, 0x61, 0x64,
-  0x67, 0x6A, 0x6D, 0x70, 0x74, 0x77, 0x7A, 0x7D
-};
+            //loKey: 1
+            //hikey: 1
+            //root: 1
+            //offset: 4
+            //sampleLen: 2
+            //loopStart: 2
+            //loopEnd: 2 
+            //decay: 2
+            //sustain: 2
+            //release: 2
 
-    // while(1)
-    // {
-    //     gpio_put_masked(0xFF00, (sine_wave[val])<<8);
-    //     // printf("Output: 0x%04x\n", (sine_wave[val])<<8);
-    //     sleep_us(8);
-    //     val++;
-    // }
+    for (uint8_t i = 0; i < map_start[0]; i++)
+    {
+        id = map_start[offset++];
+        samples = map_start[offset++];
+        printf("Patch %d, samples: %d.\n", id, samples);      
+            
+            for (uint8_t j = 0; j < samples; j++)
+            {
+                printf("\tkeys: %d - %d. Root: %d. offset: 0x%08x. length: %u\n", map_start[offset+0], map_start[offset+1], map_start[offset+2], 
+                (map_start[offset+3] + (map_start[offset+4] << 8) + (map_start[offset+5] << 16) + (map_start[offset+6] << 24)),
+                (map_start[offset+7] + (map_start[offset+8] << 8)));
+                offset += 19;
+                printf("\t\tnext offset: 0x%08x\n", offset);
+            }
+        offset+=4;
+    }
 
-    // while(1)
-    // {
-    //     if (val == 0xff)
-    //     {
-    //         count_down = true;
-    //     }
 
-    //     if (val == 0)
-    //     {
-    //         count_down = false;
-    //     }
-    //     gpio_put_masked(0xFF00, val<<8);
-    //     printf("Output: 0x%04x\n", val<<8);
-    //     sleep_ms(200);
-
-    //     if (count_down)
-    //     {
-    //         val--;
-    //     }
-    //     else
-    //     {
-    //         val++;
-    //     }
-
-    // }
 
     multicore_launch_core1(main2);
 
-    // printf("---Patches: %d---\n", map_start[0]);
-    // offset = 2 * map_start[0] + 1;
-    // for (uint8_t i = 0; i < map_start[0]; i++)
-    // {
-    //     id = map_start[offset++];
-    //     samples = map_start[offset++];
-    //     printf("Patch %d, samples: %d.\n", id, samples);      
-            
-    //         for (uint8_t j = 0; j < samples; j++)
-    //         {
-    //             printf("\tkeys: %d - %d. Root: %d. offset: 0x%08x. length: %u\n", map_start[offset+0], map_start[offset+1], map_start[offset+2], 
-    //             (map_start[offset+3] + (map_start[offset+4] << 8) + (map_start[offset+5] << 16) + (map_start[offset+6] << 24)),
-    //             (map_start[offset+7] + (map_start[offset+8] << 8)));
-    //             offset += 19;
-    //             printf("\t\tnext offset: 0x%08x\n", offset);
-    //         }
-
-    // }
 
 uint64_t start_time;
 uint64_t end_time;
